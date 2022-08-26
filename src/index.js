d17b7ffb27153cdb93f789da0184251f6c7c19f4
Mariano <marianopereyra95@gmail.com>
2022-08-25 21:47:24 -0300

    test

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider
+        domain="dev-4gyuwauo.us.auth0.com"
+        clientId="D4u81F60gFrulRSXtS0Ce21TgIt3fJRj"
+        redirectUri={window.location.origin}
+        audience="banco-austral.web.app/auth0"
+        scope="read:current_user update:current_user_metadata"
+    >
+        <App />
+    </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
